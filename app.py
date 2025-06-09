9a1b296c998d900be7a92584aa1d9aa7a7dfb18f
SergeWang <sergew027@gmail.com>
2025-06-09 11:01:25 +0800

    change file path

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ import streamlit as st
 st.write("Record thinking process")
 
 
-main_page = st.Page("main_page.py", title="Main Page", icon="")
-it_page = st.Page("it_page.py", title="information theory", icon="")
-ft_page = st.Page("ft_page.py", title="fourier transform", icon="")
+main_page = st.Page("./main_page.py", title="Main Page", icon="")
+it_page = st.Page("./it_page.py", title="information theory", icon="")
+ft_page = st.Page("./ft_page.py", title="fourier transform", icon="")
 
 # Set up navigation
 pg = st.navigation([main_page, it_page, ft_page])
